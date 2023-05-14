2ff76c9d6fb0a55d0dee0c7c943ae94093d8226d
beloyosry <byosry@gmail.com>
2023-05-14 17:55:17 +0300

    navbar & hero

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,32 @@
 import "bootstrap/dist/js/bootstrap.min.js";
-import "bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min";
 import "../sass/style.scss";
 import "@fortawesome/fontawesome-free/js/all.min.js";
+
+var navbarBrand = document.querySelector(".navbar-brand");
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 200) {
+        document.getElementById("navbar").classList.add("noTransparent");
+        navbarBrand.style.color = "#1f1f1f";
+    } else {
+        document.getElementById("navbar").classList.remove("noTransparent");
+        navbarBrand.style.color = "#fefefe";
+    }
+}
+window.onscroll = function () {
+    scrollFunction();
+};
+
+$(function () {
+    $(".nav-item").on("click", function () {
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".navbar-brand").on("click", function () {
+        $(".nav-item").removeClass("active");
+        $(".nav-item:first-child").addClass("active");
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     entry: "./src/js/index.js",
@@ -77,9 +78,34 @@ module.exports = {
         ],
     },
     plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+        }),
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             filename: "index.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/about.html",
+            filename: "about.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/contactUs.html",
+            filename: "contactUs.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/pizza-1.html",
+            filename: "pizza-1.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/pizza-2.html",
+            filename: "pizza-2.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/pizza-3.html",
+            filename: "pizza-3.html",
         }),
         new MiniCssExtractPlugin(),
     ],
