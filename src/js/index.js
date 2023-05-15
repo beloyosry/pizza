f7d263e087d9c2edbd9bcee661c1e01002e1ce2e
beloyosry <byosry@gmail.com>
2023-05-15 18:56:53 +0300

    footer

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,23 @@ import "popper.js/dist/popper.min";
 import "../sass/style.scss";
 import "@fortawesome/fontawesome-free/js/all.min.js";
 
+document.getElementById("date").innerHTML = new Date().getFullYear();
+
 var navbarBrand = document.querySelector(".navbar-brand");
+var logo = document.querySelector(".logo");
+var logoTransparent = document.querySelector(".logo-transparent");
+
 function scrollFunction() {
     if (document.documentElement.scrollTop > 200) {
         document.getElementById("navbar").classList.add("noTransparent");
         navbarBrand.style.color = "#1f1f1f";
+        logo.style.display = "none";
+        logoTransparent.style.display = "initial";
     } else {
         document.getElementById("navbar").classList.remove("noTransparent");
         navbarBrand.style.color = "#fefefe";
+        logo.style.display = "initial";
+        logoTransparent.style.display = "none";
     }
 }
 window.onscroll = function () {
@@ -35,3 +44,27 @@ $(function () {
         $(".nav-item:nth(2)").addClass("active");
     });
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    "use strict";
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+        form.addEventListener(
+            "submit",
+            (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add("was-validated");
+            },
+            false
+        );
+    });
+})();
