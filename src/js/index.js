f3f1ea23ea48352349dea740e19ebfd08038c316
beloyosry <byosry@gmail.com>
2023-05-20 02:59:10 +0300

    added chatBox

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min";
 import "../sass/style.scss";
 import "@fortawesome/fontawesome-free/js/all.min.js";
+import "/src/js/chatBox";
 
 document.getElementById("date").innerHTML = new Date().getFullYear();
 
@@ -37,64 +38,64 @@ const branchInfo = {
     التجمع: "شارع ال 90 الجنوبي أمام فندق تيوليب مول الرباط - التجمع الخامس - القاهرة",
 };
 
-function changeContent(event) {
-    // Get the text of the clicked li element
-    var branch = event.target.textContent;
-    // Get the h3 and p elements by their ids
-    var name = document.getElementById("branch-name");
-    var address = document.getElementById("branch-address");
-    // Set the innerHTML of the h3 and p elements to the branch name and address
-    name.innerHTML = branch;
-    address.innerHTML = branchInfo[branch];
-    // Get the iframe element by its id
-    var map = document.getElementById("map");
-    // Use a switch statement to change the iframe src based on the branch name
-    switch (branch) {
-        case "مدينة نصر":
-            // Change the src attribute of the iframe element to Seattle
-            map.setAttribute(
-                "src",
-                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110313.64376053694!2d31.551995174903837!3d30.22848133829782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458175c541e1e35%3A0xc6fc336786a8e571!2z2LnYqNin2LMg2KfZhNi52YLYp9iv!5e0!3m2!1sar!2seg!4v1684512167196!5m2!1sar!2seg"
-            );
-
-            break;
-        case "مصر الجديدة":
-            // Change the src attribute of the iframe element to Seattle
-            map.setAttribute(
-                "src",
-                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3452.0263085644483!2d31.318156523771094!3d30.093432866246705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e27b09a6045%3A0x3b1e67c611c406f9!2z2LPZitmG2YXYpyDYsdmI2YPYs9mK!5e0!3m2!1sar!2seg!4v1684512395429!5m2!1sar!2seg"
-            );
-            break;
-        case "المقطم":
-            // Change the src attribute of the iframe element to Seattle
-            map.setAttribute(
-                "src",
-                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.5634935692196!2d31.31669492377376!3d29.99197232105381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458392285976735%3A0xd7b1d0b0facd0d0c!2z2LTYp9ix2Lkg2KfZhNis2KfZhdi52Kkg2KfZhNit2K_Zitir2KnYjCDYp9mE2KPYqNin2KzZitip2Iwg2YLYs9mFINin2YTZhdmC2LfZhdiMINmF2K3Yp9mB2LjYqSDYp9mE2YLYp9mH2LHYqeKArA!5e0!3m2!1sar!2seg!4v1684512460381!5m2!1sar!2seg"
-            );
-            break;
-        case "الجيزة":
-            // Change the src attribute of the iframe element to Seattle
-            map.setAttribute(
-                "src",
-                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110647.61348368345!2d31.330170764198762!3d29.929446303873576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145837c586ab8ce1%3A0xc609e26e38e717f8!2z2LTYp9ix2Lkg2KfZhNmH2LHZhQ!5e0!3m2!1sar!2seg!4v1684512503064!5m2!1sar!2seg"
-            );
-            break;
-        case "التجمع":
-            // Change the src attribute of the iframe element to Seattle
-            map.setAttribute(
-                "src",
-                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27627.740797292216!2d31.503467664878638!3d30.052128391437485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458170c58240c53%3A0x4f4eb67ad149f5e2!2z2YHZhtiv2YIg2KrZiNmE2YrYqCDZgdin2YXZitmE2Yog2KjYp9ix2YM!5e0!3m2!1sar!2seg!4v1684512591530!5m2!1sar!2seg"
-            );
-            break;
-        default:
-        //Do nothing
-    }
-}
-
 // Get the ul element by its id
 var list = document.getElementById("branches");
 // Add a click event listener to the ul element
-list.addEventListener("click", changeContent);
+if (list) {
+    list.addEventListener("click", function (event) {
+        // Get the text of the clicked li element
+        var branch = event.target.textContent;
+        // Get the h3 and p elements by their ids
+        var name = document.getElementById("branch-name");
+        var address = document.getElementById("branch-address");
+        // Set the innerHTML of the h3 and p elements to the branch name and address
+        name.innerHTML = branch;
+        address.innerHTML = branchInfo[branch];
+        // Get the iframe element by its id
+        var map = document.getElementById("map");
+        // Use a switch statement to change the iframe src based on the branch name
+        switch (branch) {
+            case "مدينة نصر":
+                // Change the src attribute of the iframe element to Seattle
+                map.setAttribute(
+                    "src",
+                    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110313.64376053694!2d31.551995174903837!3d30.22848133829782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458175c541e1e35%3A0xc6fc336786a8e571!2z2LnYqNin2LMg2KfZhNi52YLYp9iv!5e0!3m2!1sar!2seg!4v1684512167196!5m2!1sar!2seg"
+                );
+
+                break;
+            case "مصر الجديدة":
+                // Change the src attribute of the iframe element to Seattle
+                map.setAttribute(
+                    "src",
+                    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3452.0263085644483!2d31.318156523771094!3d30.093432866246705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e27b09a6045%3A0x3b1e67c611c406f9!2z2LPZitmG2YXYpyDYsdmI2YPYs9mK!5e0!3m2!1sar!2seg!4v1684512395429!5m2!1sar!2seg"
+                );
+                break;
+            case "المقطم":
+                // Change the src attribute of the iframe element to Seattle
+                map.setAttribute(
+                    "src",
+                    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.5634935692196!2d31.31669492377376!3d29.99197232105381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458392285976735%3A0xd7b1d0b0facd0d0c!2z2LTYp9ix2Lkg2KfZhNis2KfZhdi52Kkg2KfZhNit2K_Zitir2KnYjCDYp9mE2KPYqNin2KzZitip2Iwg2YLYs9mFINin2YTZhdmC2LfZhdiMINmF2K3Yp9mB2LjYqSDYp9mE2YLYp9mH2LHYqeKArA!5e0!3m2!1sar!2seg!4v1684512460381!5m2!1sar!2seg"
+                );
+                break;
+            case "الجيزة":
+                // Change the src attribute of the iframe element to Seattle
+                map.setAttribute(
+                    "src",
+                    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110647.61348368345!2d31.330170764198762!3d29.929446303873576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145837c586ab8ce1%3A0xc609e26e38e717f8!2z2LTYp9ix2Lkg2KfZhNmH2LHZhQ!5e0!3m2!1sar!2seg!4v1684512503064!5m2!1sar!2seg"
+                );
+                break;
+            case "التجمع":
+                // Change the src attribute of the iframe element to Seattle
+                map.setAttribute(
+                    "src",
+                    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27627.740797292216!2d31.503467664878638!3d30.052128391437485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458170c58240c53%3A0x4f4eb67ad149f5e2!2z2YHZhtiv2YIg2KrZiNmE2YrYqCDZgdin2YXZitmE2Yog2KjYp9ix2YM!5e0!3m2!1sar!2seg!4v1684512591530!5m2!1sar!2seg"
+                );
+                break;
+            default:
+            //Do nothing
+        }
+    });
+}
 
 $(function () {
     $(".nav-link").on("click", function () {
@@ -154,6 +155,7 @@ $(function () {
         });
     });
 });
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     "use strict";
